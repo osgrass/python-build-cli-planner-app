08d131598552347e0b436e2dd0bdfb83c2e99593
osgrass <64144934+osgrass@users.noreply.github.com>
2023-04-24 14:40:26 -0400

    module 1

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from src.database import add_reminder, list_reminders
+from src.deadlined_reminder import DateReminder, DeadlinedReminder
+from src.external_reminders import EveningReminder
 
+from src.reminder import PoliteReminder
+
+DeadlinedReminder.register(PoliteReminder)
 
 def handle_input():
     choice = input("Choice: ")
@@ -12,8 +17,10 @@ def handle_input():
     elif(choice == "2"):
         print()
         reminder = input("What would you like to be reminded about?: ")
+        date = input("When is that due?:")
+        
 
-        add_reminder(reminder)
+        add_reminder(reminder, date, PoliteReminder)
         list_reminders()
     else:
         print("Invalid menu option")
@@ -33,6 +40,7 @@ def print_menu():
     print('1) List reminders')
     print('2) Add a reminder')
     print('3) Exit')
+
 
 def main():
     print_menu()
--- src/database.py
+++ src/database.py
@@ -1,5 +1,13 @@
 import csv
 
+from src.deadlined_reminder import DeadlinedReminder
+
+# import sys
+# sys.path.append("c:\\Users\\xinmwu\\Documents\\GitHub\\python-build-cli-planner-app")
+
+from src.reminder import PoliteReminder
+from src.deadlined_reminder import DateReminder
+
 def list_reminders():
     f = open("reminders.csv", "r")
 
@@ -12,8 +20,13 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text):
+def add_reminder(text, date, ReminderClass):
+    
+    if not issubclass(ReminderClass, DeadlinedReminder):
+        raise TypeError('Invalid Reminder Class')
+    
+    reminder = ReminderClass(text, date)
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow(reminder)
